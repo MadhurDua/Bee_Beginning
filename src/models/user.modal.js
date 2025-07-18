f036511c3db71626494543095e944c29758a3e1a
MadhurDua <151848669+MadhurDua@users.noreply.github.com>
2025-07-18 18:15:53 +0530

    modules done

---
+++ src/models/user.modal.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new mongoose.Schema(
+  {
+  username : {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true
+  },
+  email : {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  fullName : {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  avatar : {
+    type: String, // cloudianary URL
+    required: true
+  },
+  coverImage : {
+    type: String,
+  },
+
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref : "Video"
+    }
+  ],
+
+  password: {
+    type: String,
+    required : [true, "password is required"] 
+  },
+
+  refreshToken: {
+    type: String
+  }
+
+  }, 
+{timestamps:true}
+)
+
+
+userSchema.pre("save", async function(next) {
+  if(this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 8)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+
+
+
+
+
+export const User = mongoose.model("User", userSchema)
+
