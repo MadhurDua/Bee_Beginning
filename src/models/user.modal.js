28d6c329d2144707349cf4c7bf3144b75e11ffdc
MadhurDua <151848669+MadhurDua@users.noreply.github.com>
2025-07-25 12:05:28 +0530

    user model complete

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -64,6 +64,34 @@ userSchema.methods.isPasswordCorrect = async function(password){
   return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this.id,
+      email: this.email,
+      username: this.username,
+      fullName : this.fullName
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expireIn : process.env.ACCES_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+    {
+      _id: this.id,
+     
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expireIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 
 
 
